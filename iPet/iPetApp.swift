2fa4b9e7f912384325d82a10c1b9fd9cf019c8f5
funaika <funaika@ssfs.org>
2024-04-12 13:36:02 -0400

    Initial Commit

---
+++ iPet/iPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPetApp.swift
+//  iPet
+//
+//  Created by Kalen on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
